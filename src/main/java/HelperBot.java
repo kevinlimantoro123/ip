f839b5603e4c7fbfba4afb6e3944f18bfbe6a649
Kevin Limantoro <kevinlimantoro@Kevins-MacBook-Air-2.local>
2025-01-22 21:21:27 +0800

    Add support for echo

--- src/main/java/HelperBot.java
+++ src/main/java/HelperBot.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class HelperBot {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,10 +11,24 @@ public class HelperBot {
 
 
         String greeting = "Hello! I'm HelperBot\nWhat can I do for you?";
+        String bye = "Bye! Hope to see you again soon!";
         System.out.println(greeting);
         printHorizontalLine();
-        String bye = "Bye! Hope to see you again soon!";
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!Objects.equals(input, "bye")) {
+            printHorizontalLine();
+            System.out.println(input);
+            printHorizontalLine();
+            input = scanner.nextLine();
+        }
+        printHorizontalLine();
         System.out.println(bye);
+        printHorizontalLine();
+
+
+
     }
 
         public static void printHorizontalLine() {
